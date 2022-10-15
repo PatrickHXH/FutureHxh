a907885d064c1ceaaa39da97431d2e09776ab131
PatrickHXH <864927796@qq.com>
2022-10-15 19:51:08 +0800

    test

--- myproject/test.py
+++
@@ -1,4 +0,0 @@
-{"success": true, "error": {"code": "", "msg": ""},
- "result": {"id": 2, "project": 6, "subject": "\u6d4b\u8bd5\u62a5\u544a2022-10-14 123008",
-            "sender": " <huangxh@joysim.cn>", "receive": " <864927796@qq.com>", "report_time": "2022-10-14",
-            "email_code": 102, "report_dir": "http://localhost:8000/static/reports/VCD_UI_testReport.html"}}
