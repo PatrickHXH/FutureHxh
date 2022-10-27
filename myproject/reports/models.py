5bb71dce1ee8ee66bfb09f67c2c5c4ec3d7c9b8f
PatrickHXH <864927796@qq.com>
2022-10-27 10:18:01 +0800

    新增模型

--- myproject/reports/models.py
+++ myproject/reports/models.py
@@ -1,5 +1,8 @@
-from django.db import models
 import datetime
+
+from django.db import models
+from django.db.backends.sqlite3.base import DatabaseFeatures # 关键设置
+DatabaseFeatures.supports_microsecond_precision = False # 关键设置
 
 class EmailManage(models.Model):
     email = models.CharField("邮箱", max_length=50,null=False)
@@ -30,6 +33,7 @@ class SearchReportLog(models.Model):
     report_time = models.DateField("报告时间", null=True)
     report_dir = models.CharField("报告路径", max_length=500,null=True)
     lastest =models.BooleanField("是否最新",default=True)
+    existfail = models.BooleanField("报告是否存在错误",default=False)
     update_time = models.DateTimeField("更新时间",auto_now=True)
     create_time = models.DateTimeField("创建时间",auto_now_add=True)
 
