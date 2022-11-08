45a529cd8d0572d06671a6201660d3fb5ce9ef66
PatrickHXH <864927796@qq.com>
2022-11-08 11:49:21 +0800

    修改icon

--- vue-admin-template/src/views/settingTab/schedulerlist.vue
+++ vue-admin-template/src/views/settingTab/schedulerlist.vue
@@ -15,45 +15,39 @@
           prop="job_id"
           label="任务ID"
           width="300"
-        >
-        </el-table-column>
+        />
         <el-table-column
           prop="describe"
           label="任务描述"
           width="140"
-        >
-        </el-table-column>
+        />
         <el-table-column
           prop="trigger"
           label="触发类型"
           width="140"
-        >
-        </el-table-column>
+        />
         <el-table-column
           prop="crontab"
           label="触发时间"
           width="140"
-        >
-        </el-table-column>
+        />
         <el-table-column
           prop="excutefun"
           label="执行函数"
           width="140"
-        >
-        </el-table-column>
+        />
         <el-table-column
           prop="next_run_time"
           label="下次运行时间"
           width="180"
-        >
-        </el-table-column>
+        />
         <el-table-column
           prop="state"
           label="任务开启"
           width="140"
         >
           <template slot-scope="scope">
-            <el-switch style="margin-left: 10px"  v-model="scope.row.state" @change="switchJob(scope.row)"></el-switch>
+            <el-switch v-model="scope.row.state" style="margin-left: 10px" @change="switchJob(scope.row)" />
           </template>
         </el-table-column>
         <el-table-column label="操作">
@@ -92,7 +86,7 @@ export default {
         size: 6
       },
       total: 0,
-      switchValue:""
+      switchValue: ''
     }
   },
   mounted() {
@@ -113,7 +107,7 @@ export default {
     },
     // 获取定时任务列表
     async Schedulerlist() {
-      const resp = await SchedulerApi.joblist(this.req)
+      const resp = await SchedulerApi.joblist()
       console.log('列表', resp)
       if (resp.data.success === true) {
       // Message.success("获取列表成功")
@@ -121,11 +115,11 @@ export default {
           this.tableData.push({
             job_id: resp.data.items[i].job.id,
             describe: resp.data.items[i].describe,
-            trigger:resp.data.items[i].trigger,
-            crontab:resp.data.items[i].crontab,
-            excutefun:resp.data.items[i].excutefun,
-            next_run_time:resp.data.items[i].job.next_run_time,
-            state:resp.data.items[i].state
+            trigger: resp.data.items[i].trigger,
+            crontab: resp.data.items[i].crontab,
+            excutefun: resp.data.items[i].excutefun,
+            next_run_time: resp.data.items[i].job.next_run_time,
+            state: resp.data.items[i].state
           })
         }
         this.total = resp.data.total
@@ -133,38 +127,38 @@ export default {
         Message.error('获取列表失败')
       }
     },
-    //重启和暂停任务
-    async switchJob(row){
-      if(row.state === false){
-        const resp = await SchedulerApi.pausejob({id:row.job_id})
-        if(resp.data.success === true){
-          Message.info("暂停成功")
+    // 重启和暂停任务
+    async switchJob(row) {
+      if (row.state === false) {
+        const resp = await SchedulerApi.pausejob({ id: row.job_id })
+        if (resp.data.success === true) {
+          Message.info('暂停成功')
           this.tableData = []
           this.Schedulerlist()
-        }else{
-          Message.error("暂停失败")
+        } else {
+          Message.error('暂停失败')
         }
-      }else{
-        const resp = await SchedulerApi.resumejob({id:row.job_id})
-        if(resp.data.success === true){
-          Message.success("重启成功")
+      } else {
+        const resp = await SchedulerApi.resumejob({ id: row.job_id })
+        if (resp.data.success === true) {
+          Message.success('重启成功')
           this.tableData = []
           this.Schedulerlist()
-        }else{
-          Message.error("重启失败")
+        } else {
+          Message.error('重启失败')
         }
       }
     },
-    //删除任务
-    async DeleteJob(row){
-      const resp = await SchedulerApi.removejob({id:row.job_id})
-      if(resp.data.success === true){
-          Message.success("删除成功")
-          this.tableData = []
-          this.Schedulerlist()
-        }else{
-          Message.error("删除失败")
-        }
+    // 删除任务
+    async DeleteJob(row) {
+      const resp = await SchedulerApi.removejob({ id: row.job_id })
+      if (resp.data.success === true) {
+        Message.success('删除成功')
+        this.tableData = []
+        this.Schedulerlist()
+      } else {
+        Message.error('删除失败')
+      }
     }
   }
 }
