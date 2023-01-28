f75815cbec87b8cc7031111f342c1d4efb91495e
PatrickHXH <864927796@qq.com>
2023-01-28 13:47:09 +0800

    修改mysql配置

--- compose/mysql/init/init.sql
+++ compose/mysql/init/init.sql
@@ -1,5 +1,4 @@
 # compose/mysql/init/init.sql
 Alter user 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'patrick950207';
-#GRANT ALL PRIVILEGES ON myproject.* TO 'patrick950207'@'%';
-GRANT ALL PRIVILEGES ON *.* TO root@"121.4.54.4" identified by "patrick950207";
+GRANT ALL PRIVILEGES ON myproject.* TO 'patrick950207'@'%';
 FLUSH PRIVILEGES;
