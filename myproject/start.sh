78cce2689791f05fef629659588b495d8a2c7a8e
PatrickHXH <864927796@qq.com>
2023-02-22 16:31:06 +0800

    修改执行脚本文件

--- myproject/start.sh
+++ myproject/start.sh
@@ -13,7 +13,6 @@ done
 
 python manage.py collectstatic --noinput&&
 python manage.py makemigrations&&
-python manage.py migrate rolepermission --fake&&
 python manage.py migrate&&
 uwsgi --ini /var/www/html/myproject/uwsgi.ini&&
 tail -f /dev/null
